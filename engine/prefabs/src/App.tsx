304fa58e8541f8f23a3d9a76f534740be62c7941
Ben-Swindells <bensterbas@gmail.com>
2023-11-11 15:35:06 +0000

    feat: added new game folder with prefabs initial setup

---
+++ engine/prefabs/src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return <div className="h-screen w-screen">Prefabs</div>;
+}
+
+export default App;
---
+++ engine/scripts/README.md
@@ -0,0 +1,1 @@
+## A place to add game scripts that have no 3D output. For example, a script that controls day/night cycles, or general game code.
