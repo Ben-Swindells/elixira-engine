a0ef451fe221c4afdff3572437b2a66c1d13cbbd
Ben-Swindells <46796137+Ben-Swindells@users.noreply.github.com>
2023-12-12 21:44:18 +0000

    feat: updated cli to github

--- cli.js
+++ cli.js
@@ -6,49 +6,25 @@ const child_process = require("child_process");
 
 const args = process.argv.slice(2);
 
-function initializeProject(targetDir) {
-  const allowedFiles = [".git", "package.json"]; // Add other file/directory names if needed
-  const files = fs.readdirSync(targetDir);
-
-  // Check if directory contains files other than the allowed ones
-  const containsOtherFiles = files.some((file) => !allowedFiles.includes(file));
-  if (containsOtherFiles) {
-    console.error("Target directory contains files other than allowed ones.");
-    process.exit(1);
-  }
-
-  console.log(`Initializing project in ${targetDir}...`);
-  // Additional initialization logic here
-}
-
 function createProject(targetPath) {
   const targetDir = path.resolve(process.cwd(), targetPath);
 
-  // Create the target directory if it does not exist
-  if (!fs.existsSync(targetDir)) {
-    fs.mkdirSync(targetDir, { recursive: true });
+  // Check if the target directory is valid
+  if (fs.existsSync(targetDir) && fs.readdirSync(targetDir).length > 0) {
+    console.error("Target directory is not empty.");
+    process.exit(1);
   }
 
-  initializeProject(targetDir);
+  console.log(`Creating project in ${targetDir}...`);
 
-  console.log("Copying template files...");
-  const templateDir = path.join(__dirname, "default-template");
-  fs.readdirSync(templateDir, { withFileTypes: true }).forEach((dirent) => {
-    const sourcePath = path.join(templateDir, dirent.name);
-    const targetPath = path.join(targetDir, dirent.name);
-
-    if (dirent.isDirectory()) {
-      fs.mkdirSync(targetPath, { recursive: true });
-    } else {
-      fs.copyFileSync(sourcePath, targetPath);
-    }
+  // Clone the repository
+  const repoUrl = "https://github.com/Ben-Swindells/elixira-engine"; // Replace with your repo URL
+  child_process.execSync(`git clone ${repoUrl} "${targetDir}"`, {
+    stdio: "inherit",
   });
 
-  // Run npm install to install dependencies
-  console.log("Installing dependencies...");
-  child_process.execSync("pnpm install", { stdio: "inherit", cwd: targetDir });
+  console.log("Project created successfully.");
 }
-
 switch (args[0]) {
   case "create":
     const targetPath = args[1] || ".";
