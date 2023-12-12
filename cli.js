b284b688aa392063460fca322e2a9499072d7aa9
Ben-Swindells <46796137+Ben-Swindells@users.noreply.github.com>
2023-12-12 14:47:47 +0000

    feat: include package.json files in install

--- cli.js
+++ cli.js
@@ -7,21 +7,29 @@ const child_process = require("child_process");
 const args = process.argv.slice(2);
 
 function initializeProject(targetDir) {
+  const allowedFiles = [".git", "package.json"]; // Add other file/directory names if needed
   const files = fs.readdirSync(targetDir);
 
-  // Check if directory is empty or only contains a .git folder
-  if (files.length > 0 && !(files.length === 1 && files[0] === ".git")) {
-    console.error("Target directory is not empty.");
+  // Check if directory contains files other than the allowed ones
+  const containsOtherFiles = files.some((file) => !allowedFiles.includes(file));
+  if (containsOtherFiles) {
+    console.error("Target directory contains files other than allowed ones.");
     process.exit(1);
   }
 
-  // Copy or create essential project files (e.g., package.json, src files)
-  console.log("Creating necessary project files...");
-  // Example: fs.writeFileSync(path.join(targetDir, 'package.json'), JSON.stringify(packageJsonContent));
+  // If package.json doesn't exist, create it or copy it from a template
+  const packageJsonPath = path.join(targetDir, "package.json");
+  if (!fs.existsSync(packageJsonPath)) {
+    console.log("Creating package.json...");
+    // Example: fs.writeFileSync(packageJsonPath, JSON.stringify({/* initial content */}));
+  }
+
+  console.log(`Initializing project in ${targetDir}...`);
+  // Additional initialization logic here
 
   // Run npm install to install dependencies
   console.log("Installing dependencies...");
-  child_process.execSync("pnpm install", { stdio: "inherit", cwd: targetDir });
+  child_process.execSync("npm install", { stdio: "inherit", cwd: targetDir });
 }
 
 function createProject(targetPath) {
