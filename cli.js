cc1633d4b2c2a91d45be26ba7444fb2c25637cc5
Ben-Swindells <46796137+Ben-Swindells@users.noreply.github.com>
2023-12-12 14:30:25 +0000

    fix: update cli.js

--- cli.js
+++ cli.js
@@ -2,30 +2,43 @@
 
 const fs = require("fs");
 const path = require("path");
+const child_process = require("child_process");
 
 const args = process.argv.slice(2);
 
-function copyTemplateFiles(targetDir) {
-  const templateDir = path.join(__dirname, "template");
+function initializeProject(targetDir) {
+  // Check if the target directory is empty. If not, you might want to abort or warn the user.
+  if (fs.readdirSync(targetDir).length > 0) {
+    console.error("Target directory is not empty.");
+    process.exit(1);
+  }
 
-  fs.readdirSync(templateDir).forEach((file) => {
-    const srcFile = path.join(templateDir, file);
-    const destFile = path.join(targetDir, file);
+  // Here, add the logic to initialize your game engine environment.
+  // This might include creating configuration files, directories, etc.
+  console.log(`Initializing project in ${targetDir}...`);
 
-    fs.copyFileSync(srcFile, destFile);
-    console.log(`Created ${file}`);
-  });
+  // Example: Create a config file
+  fs.writeFileSync(
+    path.join(targetDir, "config.json"),
+    JSON.stringify({
+      /* initial config data */
+    })
+  );
+
+  // If your setup includes installing dependencies or running scripts, do it here
+  // For example, to run 'npm install':
+  child_process.execSync("npm install", { stdio: "inherit", cwd: targetDir });
 }
 
 function createProject(targetPath) {
   const targetDir = path.resolve(process.cwd(), targetPath);
 
+  // Create the target directory if it does not exist
   if (!fs.existsSync(targetDir)) {
     fs.mkdirSync(targetDir, { recursive: true });
   }
 
-  console.log(`Creating project in ${targetDir}...`);
-  copyTemplateFiles(targetDir);
+  initializeProject(targetDir);
 }
 
 switch (args[0]) {
