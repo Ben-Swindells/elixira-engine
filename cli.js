5ef95f9b8d905609c164da125855f770137fb157
Ben-Swindells <46796137+Ben-Swindells@users.noreply.github.com>
2023-12-12 14:36:19 +0000

    fix: update cli.js

--- cli.js
+++ cli.js
@@ -7,23 +7,16 @@ const child_process = require("child_process");
 const args = process.argv.slice(2);
 
 function initializeProject(targetDir) {
-  // Check if the target directory is empty. If not, you might want to abort or warn the user.
-  if (fs.readdirSync(targetDir).length > 0) {
+  const files = fs.readdirSync(targetDir);
+
+  // Check if directory is empty or only contains a .git folder
+  if (files.length > 0 && !(files.length === 1 && files[0] === ".git")) {
     console.error("Target directory is not empty.");
     process.exit(1);
   }
 
-  // Here, add the logic to initialize your game engine environment.
-  // This might include creating configuration files, directories, etc.
   console.log(`Initializing project in ${targetDir}...`);
-
-  // Example: Create a config file
-  fs.writeFileSync(
-    path.join(targetDir, "config.json"),
-    JSON.stringify({
-      /* initial config data */
-    })
-  );
+  // Your initialization logic here
 
   // If your setup includes installing dependencies or running scripts, do it here
   // For example, to run 'npm install':
