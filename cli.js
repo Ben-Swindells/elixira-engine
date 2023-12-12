8a8c4aface843303edb32db32dbdc0f4c2d9fe7d
Ben-Swindells <46796137+Ben-Swindells@users.noreply.github.com>
2023-12-12 14:23:28 +0000

    feat: updated cli script to execute installation

--- cli.js
+++ cli.js
@@ -1,6 +1,39 @@
 #!/usr/bin/env node
-// cli.js
 
-console.log("Installing the Elixira game engine, please wait...");
+const fs = require("fs");
+const path = require("path");
 
-// Your installation logic here
+const args = process.argv.slice(2);
+
+function copyTemplateFiles(targetDir) {
+  const templateDir = path.join(__dirname, "template");
+
+  fs.readdirSync(templateDir).forEach((file) => {
+    const srcFile = path.join(templateDir, file);
+    const destFile = path.join(targetDir, file);
+
+    fs.copyFileSync(srcFile, destFile);
+    console.log(`Created ${file}`);
+  });
+}
+
+function createProject(targetPath) {
+  const targetDir = path.resolve(process.cwd(), targetPath);
+
+  if (!fs.existsSync(targetDir)) {
+    fs.mkdirSync(targetDir, { recursive: true });
+  }
+
+  console.log(`Creating project in ${targetDir}...`);
+  copyTemplateFiles(targetDir);
+}
+
+switch (args[0]) {
+  case "create":
+    const targetPath = args[1] || ".";
+    createProject(targetPath);
+    break;
+  // Additional cases for other commands
+  default:
+    console.log("Command not recognized");
+}
